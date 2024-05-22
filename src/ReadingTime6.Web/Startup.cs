d42f457c2a27ae37a0e02b15fdf41ec35cb38ab0
louis979 <170572075+louis979@users.noreply.github.com>
2024-05-22 23:13:25 +0000

    aaaa

--- src/ReadingTime6.Web/Startup.cs
+++ src/ReadingTime6.Web/Startup.cs
@@ -4,15 +4,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+ 
+    var pat = "{put your pat here}";
 namespace ReadingTime6.Web
 {
+ 
+   
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
